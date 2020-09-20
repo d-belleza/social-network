a421d116ce02742325be930f7f03c044caaed744
davidbelleza <davidbelleza@gmail.com>
2020-09-20 16:39:41 -0500

    add Models

--- models/User.js
+++ models/User.js
@@ -3,19 +3,39 @@ const { Schema, model } = require('mongoose');
 const UserSchema = new Schema(
     {
         username: {
-
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
         },
         email: {
-
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Enter a valid email address!']
         },
         thoughts: {
-
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
         },
-        friends: {
-
-        }
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
     }
 );
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
 
 const User = model('User', UserSchema);
 
